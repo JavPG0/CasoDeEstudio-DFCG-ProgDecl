a6cc5bc32b08d2aca9aa55086ec9ba0dda2e971f
David Dionisio Laureano <David.Dionisio@alu.uclm.es>
2026-04-08 01:22:48 +0200

    Add files via upload
    Añadido algunos cambios antes de la reunión 2

---
+++ src/Project(1).pl
@@ -0,0 +1,74 @@
+% --- CAMBIO: Oración = SN + SV + Puntuacion ---
+oracion --> sujeto, predicado, puntuacion.
+
+sujeto --> sintagma_nominal.
+predicado --> sintagma_verbal, complemento.
+predicado --> sintagma_verbal.
+
+    % Puntuacion final
+puntuacion --> ['.'].
+    % ----------------------------------------------
+
+    % Sintagma nominal: articulo + sustantivo [+ sintagma adjetival] [+ sintagma preposicional]
+sintagma_nominal --> articulo, sintagma_nominal2.
+sintagma_nominal --> sustantivo.
+sintagma_nominal2 --> sustantivo.
+sintagma_nominal2 --> sustantivo , sintagma_adjetival.
+sintagma_nominal2 --> sustantivo, sintagma_preposicional.
+
+    % Sintagma verbal: verbo
+sintagma_verbal --> verbo.
+
+    % Complemento opcional = SN
+complemento --> sintagma_nominal.
+complemento --> sintagma_nominal, complemento.
+complemento --> sintagma_adjetival.
+complemento --> sintagma_adjetival, complemento.
+complemento --> sintagma_preposicional.
+complemento --> sintagma_preposicional, complemento.
+
+    % Sintagma adjetival = adjetivo
+sintagma_adjetival --> adjetivo.
+
+    % Sintagma preposicional = prep + SN
+sintagma_preposicional --> preposicion, sintagma_nominal.
+ 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+articulo --> [el].
+articulo --> [la].
+articulo --> [los].
+articulo --> [las].
+articulo --> [su].
+
+sustantivo --> [niño].
+sustantivo --> [niña].
+sustantivo --> [perro].
+sustantivo --> [perros].
+sustantivo --> [casa].
+ 
+adjetivo --> [pequeño].
+adjetivo --> [pequeña].
+adjetivo --> [rápido].
+adjetivo --> [rápida].
+adjetivo --> [lento].
+adjetivo --> [lenta].
+
+verbo --> [corre].
+verbo --> [salta].
+verbo --> [come].
+verbo --> [ve].
+
+preposicion --> [en].
+preposicion --> [con].
+preposicion --> [sin].
+preposicion --> [sobre].
+preposicion --> [bajo].
+preposicion --> [entre].
+preposicion --> [hacia].
+preposicion --> [desde].
+preposicion --> [de].
+preposicion --> [entre].
+preposicion --> [hacia].
+preposicion --> [desde].
+preposicion --> [de].
